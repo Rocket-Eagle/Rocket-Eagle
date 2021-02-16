9e58fa5a82be3fda094c948054ea56e0e781c0a0
joebobyjoe <59772194+joeboby@users.noreply.github.com>
2021-02-16 11:42:29 -0600

    Added Loading Scene
    Added a (very) basic loading screen and loading bar to use for loading new scenes

---
+++ rocket_eagle/Assets/Scripts/SceneLoading.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoading : MonoBehaviour
+{
+    [SerializeField]private Image _progressBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //start the async operation
+        StartCoroutine(LoadAsyncOperation());
+    }
+
+    IEnumerator LoadAsyncOperation()
+    {
+        //get the async operation manager
+        AsyncOperation gameLevel = SceneManager.LoadSceneAsync(1);
+
+
+        while(gameLevel.progress < 1)
+        {
+            _progressBar.fillAmount = gameLevel.progress;
+            yield return new WaitForEndOfFrame();
+        }
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

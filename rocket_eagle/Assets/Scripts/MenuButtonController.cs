b6f00d675775639120b99a42d830b5a8acfc9fb9
joebobyjoe <59772194+joeboby@users.noreply.github.com>
2021-02-22 14:16:51 -0600

    Small refactoring
    removed small pieces of unused code
    Renamed sound FX to be more descriptive

--- rocket_eagle/Assets/Scripts/MenuButtonController.cs
+++ rocket_eagle/Assets/Scripts/MenuButtonController.cs
@@ -7,10 +7,8 @@ using UnityEngine.Audio;
 public class MenuButtonController : MonoBehaviour {
 
 	// Use this for initialization
-	[SerializeField] int maxIndex;
 	public AudioSource audioSource;
 	public bool disableOnce;
-
 
 
 	void Start () 
@@ -21,48 +19,6 @@ public class MenuButtonController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{		
-		
-	}
-
-	/*
-	 * this is the function that is called when the buttons on the main menu are pressed
-	 * the index for the animation is set and then the buttons are executed.
-	 * 
-	 */
-	public void ButtonPressed(int buttonIndex)
-	{
-		switch (buttonIndex)
-		{
-			case 0:
-				//clicked on the single player option
-				Debug.Log("Clicked: SP Menu");
-
-				//launch the SP sub menu
-				break;
-			case 1:
-				//clicked on the multi player option
-				Debug.Log("Clicked: MP Menu");
-
-				//launch the MP sub menu
-				break;
-			case 2:
-				//clicked on the customize option
-				Debug.Log("Clicked: CUST Menu");
-
-				//launch the customization sub menu
-				break;
-			case 3:
-				//clicked on the options option
-				Debug.Log("Clicked: OP Menu");
-
-				//launch the options sub menu
-				break;
-
-			default:
-					Debug.Log("WARNING! Button was selected that doesn't have a proper function with it!");
-					Debug.Log("The Index was:" + buttonIndex);
-				break;
-		}
 		
 	}
 
